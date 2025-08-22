d87b89a1855f17bf79b5ab6a65f1368235c98077
leoplute <leoplute@gmail.com>
2025-08-22 12:01:39 -0600

    Cleaned up code, think it should be good for submission now.

--- app/src/main/java/com/example/homework1/MainActivity.kt
+++ app/src/main/java/com/example/homework1/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.homework1
 
 import android.os.Bundle
 import android.content.Intent
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -14,65 +13,33 @@ class MainActivity : AppCompatActivity() {
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
 
-        //enableEdgeToEdge()
-        /*
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
-
         // Bind the pizza button to switch screens and display 'pizza' on new screen
-        binding.pizzaButton.setOnClickListener {
-            val buttonText = binding.pizzaButton.text.toString()
-            val intent = Intent(this, SecondActivity::class.java)
-            val argsBundle = Bundle()
-            argsBundle.putString("text", buttonText)
-            intent.putExtras(argsBundle)
-            startActivity(intent)
-        }
+        createClickListener(binding.pizzaButton)
 
         // Bind the bacon button to switch screens and display 'bacon' on new screen
-        binding.baconButton.setOnClickListener {
-            val buttonText = binding.baconButton.text.toString()
-            val intent = Intent(this, SecondActivity::class.java)
-            val argsBundle = Bundle()
-            argsBundle.putString("text", buttonText)
-            intent.putExtras(argsBundle)
-            startActivity(intent)
-        }
+        createClickListener(binding.baconButton)
 
         // Bind the apple button to switch screens and display 'apple' on new screen
-        binding.appleButton.setOnClickListener {
-            val buttonText = binding.appleButton.text.toString()
-            val intent = Intent(this, SecondActivity::class.java)
-            val argsBundle = Bundle()
-            argsBundle.putString("text", buttonText)
-            intent.putExtras(argsBundle)
-            startActivity(intent)
-        }
+        createClickListener(binding.appleButton)
 
         // Bind the chips button to switch screens and display 'chips' on new screen
-        binding.chipsButton.setOnClickListener {
-            val buttonText = binding.chipsButton.text.toString()
-            val intent = Intent(this, SecondActivity::class.java)
-            val argsBundle = Bundle()
-            argsBundle.putString("text", buttonText)
-            intent.putExtras(argsBundle)
-            startActivity(intent)
-        }
+        createClickListener(binding.chipsButton)
 
         // Bind the cheeseburger button to switch screens and display 'cheeseburger' on new screen
-        binding.burgerButton.setOnClickListener {
-            val buttonText = binding.burgerButton.text.toString()
+        createClickListener(binding.burgerButton)
+
+        setContentView(binding.root)
+    }
+
+    fun createClickListener(button : android.widget.Button){
+        button.setOnClickListener {
+            val buttonText = button.text.toString()
             val intent = Intent(this, SecondActivity::class.java)
             val argsBundle = Bundle()
             argsBundle.putString("text", buttonText)
             intent.putExtras(argsBundle)
             startActivity(intent)
         }
-
-        setContentView(binding.root)
-
     }
+
 }
