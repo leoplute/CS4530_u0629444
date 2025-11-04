fd8e5c013e8a1e4920e69f44e62cc5755bc468ac
leoplute <leoplute@gmail.com>
2025-11-03 19:45:26 -0700

    Added more to architecture stuff. Need to add logic + frontend still.

--- assignment5/app/src/main/java/com/example/assignment5/MarbleRepository.kt
+++ assignment5/app/src/main/java/com/example/assignment5/MarbleRepository.kt
@@ -1,9 +1,36 @@
 package com.example.assignment5
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.Flow
 
-data class MarbleData(val x : Float = 0f, val y : Float = 0f, val velocityX : Float = 0f, val velocityY : Float = 0f)
+data class MarbleData(val x : Float, val y : Float, val z : Float)
 
 class MarbleRepository(private val sensorManager : SensorManager) {
+
+    fun getGravityFlow() : Flow<MarbleData> = channelFlow {
+        val gravSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
+        val sensor = gravSensor ?: sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        if(sensor == null){
+            return@channelFlow
+        }
+
+        val listener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent) {
+                trySendBlocking(MarbleData(event.values[0], event.values[1], event.values[2]))
+            }
+
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy : Int) = Unit
+        }
+
+        sensorManager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_GAME)
+        awaitClose { sensorManager.unregisterListener(listener) }
+    }
 
 }
--- assignment5/app/src/main/java/com/example/assignment5/MarbleViewModel.kt
+++ assignment5/app/src/main/java/com/example/assignment5/MarbleViewModel.kt
@@ -1,9 +1,29 @@
 package com.example.assignment5
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 
 class MarbleViewModel(private val repo : MarbleRepository) : ViewModel(){
 
-    val marbleReading = repo.get
+    val marbleReading = repo.getGravityFlow()
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000),
+            MarbleData(0f, 0f, 0f)
+        )
+
+    companion object {
+        val Factory = viewModelFactory {
+            initializer {
+                val application = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MarbleApp)
+                MarbleViewModel(application.marbleRepository)
+            }
+        }
+    }
 
 }
